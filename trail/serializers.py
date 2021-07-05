efefc20a481138947826da0fff2d15683f5bb4f8
samorgan32 <samorgan32@gmail.com>
2021-07-05 14:02:57 -0500

    walkthrough model contains nested slide models

--- trail/serializers.py
+++ trail/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 from .models import Walkthrough, Slide
 
+class SlideSerializer(serializers.ModelSerializer):
+    walkthrough = serializers.SlugRelatedField(
+        queryset = Walkthrough.objects.all(),
+        slug_field='pk'
+    )
+
+    walkthrough_owner = serializers.ReadOnlyField(source='walkthrough.owner.username')
+
+    class Meta:
+        model = Slide
+        fields = ('id', 'image', 'description', 'walkthrough', 'position', 'walkthrough_owner')
+        
 class WalkthroughSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(
         source = 'users.User',
@@ -13,20 +25,11 @@ class WalkthroughSerializer(serializers.ModelSerializer):
         read_only = True
     )
 
+    slides = SlideSerializer(many=True, read_only=True)
+
     class Meta:
         model = Walkthrough
-        fields = ('id', 'title', 'owner', 'date_created', 'cover_slide', 'slide_images')
+        fields = ('id', 'title', 'owner', 'date_created', 'cover_slide', 'slide_images', 'slides')
 
 
 
-class SlideSerializer(serializers.ModelSerializer):
-    walkthrough = serializers.SlugRelatedField(
-        queryset = Walkthrough.objects.all(),
-        slug_field='pk'
-    )
-
-    walkthrough_owner = serializers.ReadOnlyField(source='walkthrough.owner.username')
-
-    class Meta:
-        model = Slide
-        fields = ('id', 'image', 'description', 'walkthrough', 'position', 'walkthrough_owner')
